103e6daa8ee31379a0972290d25090c6b3ae9870
savag3 <savagetism@icloud.com>
2026-09-20 13:09:54 -0400

    bad-apple 0.3.1

--- Casks/bad-apple.rb
+++ Casks/bad-apple.rb
@@ -1,7 +1,7 @@
 cask "bad-apple" do
-  version "0.3.0"
+  version "0.3.1"
   # Update this sha256 for each release. package_minimal_release.sh prints the final hash.
-  sha256 "5d6df06a2569b7c73f32cdbbfd9c2dbc610f389aca2c73e95496cf6ae7051bb6"
+  sha256 "d74f0cf218fb899d2caeb6219d6e2f59fb2d72b1b062c4a5834cc0ab620fc17d"
 
   url "https://github.com/savageAZfck/bad-apple-releases/releases/download/v#{version}/Bad_Apple-#{version}-unsigned.zip"
   name "Bad Apple"
